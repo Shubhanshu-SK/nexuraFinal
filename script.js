ae33473d310c8ec9b36fd75fa173ad8416ace277
aryan87701 <singharyan792005@gmail.com>
2025-03-10 00:46:31 +0530

    main page + nav bar updated

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ document.addEventListener("DOMContentLoaded", function () {
     })
 
 })
+const hamMenu = document.querySelector(".ham-menu");
+const offScreenMenu = document.querySelector(".off-screen-menu");
+const hamli=document.querySelector(".hamli");
+const hamlj=document.querySelector(".hamlj");
+const hamlk=document.querySelector(".hamlk");
+const hamll=document.querySelector(".hamll");
+const hamlm=document.querySelector(".hamlm");
+const hamln=document.querySelector(".hamln");
+const hamlo=document.querySelector(".hamlo");
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+  hamli.classList.toggle("active");
+  hamlj.classList.toggle("active");
+  hamlk.classList.toggle("active");
+  hamll.classList.toggle("active");
+  hamlm.classList.toggle("active");
+  hamln.classList.toggle("active");
+  hamlo.classList.toggle("active");
+});
+
